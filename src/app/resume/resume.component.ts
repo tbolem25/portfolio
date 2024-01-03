1cb2b2c0288570f2d7357ebc19f655b35db47cb8
tbolem25 <tejabolem25@gmail.com>
2024-01-03 15:21:33 -0500

    Exp started shaping

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import {CommonModule} from "@angular/common";
 interface Project {
   companyName: string;
+  roleName: string;
   timePeriod: string;
   workPoints: string[];
 }
@@ -10,20 +11,22 @@ interface Project {
   standalone: true,
   imports: [CommonModule],
   templateUrl: './resume.component.html',
-  styleUrl: './resume.component.sass'
+  styleUrl: './resume.component.sass',
+  encapsulation: ViewEncapsulation.None  // Disable view encapsulation
 })
 export class ResumeComponent {
   projects: Project[] = [
     {
-      companyName: 'Company 1',
-      timePeriod: '2022-2023',
-      workPoints: ['Work point 1', 'Work point 2']
+      companyName: 'Prudential Financial, NY',
+      roleName: 'Sr.Full Stack Java Developer',
+      timePeriod: ' Jun 2022 - Present',
+      workPoints: ['Prudential Financial is a distinguished financial corporation, and I am a valued member of the Card Services team, contributing as a Full Stack Java Developer enhancing the current software.']
     },
     {
-      companyName: 'Company 2',
-      timePeriod: '2024-2026',
-      workPoints: ['Work point 1', 'Work point 2']
+      companyName: 'Abbott Laboratories, IL',
+      roleName: 'Sr.Full Stack Java Developer',
+      timePeriod: 'Sep 2021 - May 2022',
+      workPoints: ['Abbott Laboratories is a prominent healthcare provider renowned for its diverse range of healthcare-related machinery. As a Full Stack Java Developer, I developed a comprehensive software designed to monitor machine status, health, and error diagnostics.']
     },
-    // ... add more projects here
   ];
 }
